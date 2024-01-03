a75810a66bfe08d5de8a50359f03c1d8de7397ee
William Stam <williamstam@gmail.com>
2024-01-03 18:06:08 +0100

    [script.js] Remove comments and styling, Change name of div

--- script.js
+++ script.js
@@ -5,14 +5,6 @@ const resetSearch = () => {
   search.reset()
 }
 
-// function fetchData() {
-//   fetch('../../data.json')
-//     .then((res) => {
-//       return res.json()
-//     })
-//     .then((data) => console.log(data.title))
-// }
-
 function fetchData() {
   fetch('../../data.json')
     .then(function (response) {
@@ -21,29 +13,22 @@ function fetchData() {
     .then(function (json) {
       for (let i = 0; i < json.length; i++) {
         if (json[i].isTrending) {
-          // Append FROM SECTION, GIVE SECTION ID AND APPEND THERE
-          let divElement = document.createElement('Div')
-          divElement.id = 'divID'
-
-          divElement.style.textAlign = 'center'
-          divElement.style.fontWeight = 'bold'
-          divElement.style.fontSize = 'smaller'
-          divElement.style.paddingTop = '15px'
+          let trendingItemContainer = document.getElementById('trendingItems')
+          let trendingItem = document.createElement('Div')
+          trendingItem.classList.add('trendingItem')
 
           let paragraph = document.createElement('P')
           let text = document.createTextNode(`${json[i].title}`)
 
           paragraph.appendChild(text)
-          divElement.appendChild(paragraph)
+          trendingItem.appendChild(paragraph)
 
           let trendingImg = document.createElement('img')
           trendingImg.src = `${'.' + json[i].thumbnail.trending.small}`
           trendingImg.classList.add('trendingImgContainer')
 
-          divElement.appendChild(trendingImg)
-
-          // Appending the div element to body
-          trending.appendChild(divElement)
+          trendingItem.appendChild(trendingImg)
+          trendingItemContainer.appendChild(trendingItem)
         }
       }
     })
